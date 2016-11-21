5e2be3b7795dff9953112b9857b1003ae8dbf35e
bsrikant <srikant.bondugula@ignitesol.com>
2016-11-21 18:21:03 +0530

    testing git 2

--- email_class.py
+++ email_class.py
@@ -18,6 +18,7 @@ import time
 def get_unique_words(doc):
     # define splitter with non-alphabetic chars
     regex_str = r'[\W]+'
+    #dddd
     splitter = re.compile(regex_str)
     # split documents with splitter as separator
     words = [s.lower() for s in re.split(splitter, doc) if len(s)>2 and len(s)<20]
