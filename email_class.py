fa0a6b5ccc2aafcf8246569c1090b39713c2417d
bsrikant <srikant.bondugula@ignitesol.com>
2016-11-26 11:39:58 +0530

    added training and testing function

--- email_class.py
+++ email_class.py
@@ -9,11 +9,50 @@ import sqlalchemy
 import os
 from scipy import stats
 from sklearn.model_selection import train_test_split
+from joblib import Parallel, delayed
+import time
+
 
 
 pd.set_option('display.max_columns',5)
-pd.set_option('display.max_rows',20)
+pd.set_option('display.max_rows',24)
 pd.set_option('expand_frame_repr',False)
+
+
+# train it ##########################################################
+def train_classifier(cl, X_train, y_train):
+    for i, (rowidx, file_path) in enumerate(X_train.iteritems()):
+        try:
+            with open(file_path, 'r') as txt_file:
+                txt = txt_file.read()
+        except UnicodeDecodeError:
+            continue
+        category = y_train[rowidx]
+        cl.train(txt, [category])
+        if not bool((i + 1) % 100):
+            print('Trained on', i + 1, 'files', flush=True)
+    return
+
+
+# predict_categories ##############################################################################
+def predict_categories(cl, X_test):
+    df_test = pd.DataFrame(index=X_test.index)
+    df_test['Pred_One_Category'] = '-'
+    df_test['Pred_Multi_Category'] = '-'
+    df_test['Accuracy_One_Category'] = 0
+    df_test['Accuracy_Multi_Category'] = 0
+    for i, (rowidx, file_path) in enumerate(X_test.iteritems()):
+        try:
+            with open(file_path, 'r') as txt_file:
+                txt = txt_file.read()
+        except UnicodeDecodeError:
+            continue
+        p_categories, top_categories = cl.classify(txt, threshold=2)
+        df_test.set_value(rowidx, 'Pred_Multi_Category', top_categories)
+        df_test.set_value(rowidx, 'Pred_One_Category', top_categories[0])
+        # if not bool((i + 1) % 100):
+        #     print('Tested on', i + 1, 'files', flush=True)
+    return df_test
 
 
 # walk through and list files in DataSet folder ###################################################
@@ -43,45 +82,30 @@ def train_test_on_datadir(cl, dir_name='20_newsgroup', samplefrac=0.2, randstate
     print('Training Sample Size:', n_train)
     print('Testing Sample Size:', n_test)
 
-    # train it ##########################################################
-    def train_classifier(cl, X_train, y_train):
-        print('\nTraining ...')
-        for i, (rowidx, file_path) in enumerate(X_train.iteritems()):
-            try:
-                with open(file_path, 'r') as txt_file:
-                    txt = txt_file.read()
-            except UnicodeDecodeError:
-                continue
-            category = y_train[rowidx]
-            cl.train(txt,[category])
-            if not bool((i + 1) % 100):
-                print('Trained on', i + 1, 'files', flush=True)
-
-    # use it ###########################################################
-    def use_classifier(cl, X_test):
-        print('\nTesting ...')
-        df_test = pd.DataFrame(index=X_test.index)
-        df_test['Pred_One_Category'] = '-'
-        df_test['Pred_Multi_Category'] = '-'
-        df_test['Accuracy_One_Category'] = 0
-        df_test['Accuracy_Multi_Category'] = 0
-        for i, (rowidx, file_path) in enumerate(X_test.iteritems()):
-            try:
-                with open(file_path, 'r') as txt_file:
-                    txt = txt_file.read()
-            except UnicodeDecodeError:
-                continue
-            p_categories, top_categories = cl.classify(txt, threshold=2)
-            df_test.set_value(rowidx,'Pred_Multi_Category', top_categories)
-            df_test.set_value(rowidx,'Pred_One_Category', top_categories[0])
-            if not bool((i + 1) % 100):
-                print('Tested on', i + 1, 'files', flush=True)
-        return df_test
-
+    # train the classifier on training dataset
+    print('\nTraining ...')
+    t1 = time.time()
     train_classifier(cl, X_train, y_train)
-    df_test = use_classifier(cl, X_test)
+    t2 = time.time()
+    print('Finished Training on %d' % n_train, 'items in', t2-t1, 'seconds')
+    print('Total number of items used:', cl.ds_category_count.sum())
+    print('Number of items trained, by category:')
+    print(cl.ds_category_count)
 
-    # find accuracy
+    # test the classifier on testing dataset
+    print('\nTesting ...')
+    t1 = time.time()
+    n_jobs = 4 # number of parallel jobs
+    parallelizer = Parallel(n_jobs)
+    parts_X_test = np.array_split(X_test,n_jobs)
+    tasks_iterator = ( delayed(predict_categories)(cl, part_X) for part_X in  parts_X_test)
+    list_df_test = parallelizer(tasks_iterator)
+    df_test = pd.concat(list_df_test,axis=0)
+    # df_test = predict_categories(cl,X_test)
+    t2 = time.time()
+    print('Finished Classification of %d' % n_test, 'items in', t2-t1, 'seconds')
+
+    # find accuracy of classification
     df_test['True_Category'] = y_test
     def check_single_accuracy(row):
         return int( row['True_Category'] == row['Pred_One_Category'] )
@@ -89,6 +113,7 @@ def train_test_on_datadir(cl, dir_name='20_newsgroup', samplefrac=0.2, randstate
         return int( row['True_Category'] in row['Pred_Multi_Category'] )
     df_test['Accuracy_One_Category'] = df_test.apply(check_single_accuracy, axis=1)
     df_test['Accuracy_Multi_Category'] = df_test.apply(check_multi_accuracy, axis=1)
+
     # return accuracy and predictions
     return df_test[['Accuracy_One_Category','Accuracy_Multi_Category']].sum()/n_test,\
            df_test[['True_Category','Pred_One_Category','Pred_Multi_Category',\
